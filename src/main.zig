d25b86207c6296aeb290a6e3a1f78c5dc08c50a8
Ryan d'Huart <homoelectromagneticus@protonmail.com>
2025-01-03 17:25:10 -0500

    The left-hand-side index is now accurate
    Still need to figure out how to handle the case where the buffer is
    either larger or smaller than a single line. Right now, those cases
    produce garbage in the output.
    
    Now the program opens an input file in read-only mode.
    
    There is an integer overflow if the number of columns to print is odd! I
    need to fix that.

--- src/main.zig
+++ src/main.zig
@@ -7,11 +7,9 @@ const printParams = struct {
     group_size: usize = 2,
 };
 
-// TODO: figure out how to handle an input spanning multiple buffers
-fn print_output(writer: anytype, params: printParams, input: []const u8, input_length: usize) !void {
-
-    // where in the input does the line start
-    var line_start_position: usize = 0;
+fn print_columns(writer: anytype, params: printParams, input: []const u8) !usize {
+    // amount of bytes converted into hex
+    var num_printed_bytes: usize = 0;
     // number of printed characters of translated input (not including the
     // index markers on the left!)
     var num_printed_chars: usize = 0;
@@ -22,62 +20,62 @@ fn print_output(writer: anytype, params: printParams, input: []const u8, input_l
             try writer.print(" ", .{});
             num_printed_chars += 1;
         }
-        // check to see if we are at the end of a column (or at the very first
-        // character of the input)
-        if (index % params.num_columns == 0) {
-            // if so, print the slice of the string the columns on the line
-            // represent
-            for (input[line_start_position..index]) |raw_char| {
-                if ('\n' == raw_char or '\t' == raw_char) {
-                    try writer.print(".", .{});
-                } else {
-                    try writer.print("{c}", .{raw_char});
-                }
-            }
-            try writer.print("\n", .{});
-            line_start_position = index;
-            num_printed_chars = 0;
-            // add the index for the next line
-            try writer.print("{x:0>8}: ", .{index});
-        }
         // print the hex value of the current character
         try writer.print("{x:0>2}", .{character});
+        num_printed_bytes += 1;
         num_printed_chars += 2;
 
         // if we are at the end of the input
-        if (index == input_length - 1) {
+        if (index == input.len - 1) {
             // figure out how many spaces to put in so that the text lines up
             // nicely
-            const num_spaces: usize = params.line_length - num_printed_chars;
+            const num_spaces: usize = 1 + params.line_length - num_printed_chars;
             for (0..num_spaces) |_| {
                 try writer.print(" ", .{});
             }
 
-            for (input[line_start_position..input_length]) |raw_char| {
+            for (input) |raw_char| {
                 if ('\n' == raw_char or '\t' == raw_char) {
                     try writer.print(".", .{});
                 } else {
                     try writer.print("{c}", .{raw_char});
                 }
             }
-            break;
         }
     }
+    return num_printed_bytes;
+}
+
+fn print_output(writer: anytype, params: printParams, input: []const u8, file_pos: usize) !usize {
+    var new_file_pos = file_pos;
+
+    // split the buffer into segments based on the number of columns specified
+    // via the command line arguments (or left as default (16))
+    var input_iterator = std.mem.window(u8, input, params.num_columns, params.num_columns);
+
+    // loop through the buffer and print the output in chunks of the columns
+    while (input_iterator.next()) |slice| {
+        // print the file position
+        try writer.print("{x:0>8}:", .{new_file_pos});
+        new_file_pos += try print_columns(writer, params, slice);
+        try writer.print("\n", .{});
+    }
+    return new_file_pos;
 }
 
 pub fn main() !void {
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
+    // stdout is for the actual output of your application
     const stdout_file = std.io.getStdOut().writer();
     var bw = std.io.bufferedWriter(stdout_file);
     const stdout = bw.writer();
 
+    // need to allocate memory in order to parse the command-line args, handle
+    // buffers, etc
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
 
     // First we specify what parameters our program can take.
-    // We can use `parseParamsComptime` to parse a string into an array of `Param(Help)`.
+    // We use `parseParamsComptime` to parse a string into an array of `Param(Help)`.
     const params = comptime clap.parseParamsComptime(
         \\-h, --help              Display this help and exit.
         \\-c, --columns <usize>   Format <columns> per line, default is 16, max 256
@@ -102,47 +100,47 @@ pub fn main() !void {
     defer res.deinit();
 
     // store details about how we are printing in this struct
-    var current_print_params = printParams{};
+    var print_params = printParams{};
     // setting the number of columns to use when printing (default 16)
     if (res.args.columns) |c| {
-        current_print_params.num_columns = c;
+        print_params.num_columns = c;
     }
     // setting the output grouping when printing (default 2)
     if (res.args.groupsize) |g| {
-        current_print_params.group_size = g;
+        print_params.group_size = g;
     }
-    current_print_params.line_length =
-        ((current_print_params.group_size * 2) + 1) *
-        (current_print_params.num_columns / current_print_params.group_size);
+    print_params.line_length =
+        ((print_params.group_size * 2) + 1) *
+        (print_params.num_columns / print_params.group_size);
 
     // if we have a simple input string
     if (res.args.string) |s| {
-        try print_output(stdout, current_print_params, s, s.len);
+        _ = try print_output(stdout, print_params, s, s.len);
     }
 
+    // TODO: figure out how to handle and input spanning multiple buffers
     // if we have an input file
     if (res.args.file) |f| {
         // Interpret the filepath
         var path_buffer: [std.fs.MAX_PATH_BYTES]u8 = undefined;
         const path = try std.fs.realpath(f, &path_buffer);
 
-        // open the file
-        // The `.{}` means use the default version of `File.OpenFlags`.
-        const file = try std.fs.openFileAbsolute(path, .{});
+        // open the file in read-only mode
+        const file = try std.fs.openFileAbsolute(path, .{ .mode = .read_only });
         defer file.close();
 
-        // stream the file into memory and get the contents one chunk at a time
+        // buffer the file into memory and get the contents one chunk at a time
         var buffered_file = std.io.bufferedReader(file.reader());
-        var file_contents_buffer: [4096]u8 = undefined;
+        var file_contents_buffer: [32]u8 = undefined;
 
-        // TODO: keep track of how many lines have been printed so that the
-        //       printed index is accurate. maybe printing the index should be
-        //       handled outside the print_output function?
+        // we'll need to keep track of where we are in the file
+        var file_pos: usize = 0;
+
         // load data into the buffer and print it until the buffer is empty
         while (true) {
             const number_of_bytes_read: usize = try buffered_file.read(&file_contents_buffer);
             if (number_of_bytes_read == 0) break; //no more data to read
-            try print_output(stdout, current_print_params, &file_contents_buffer, number_of_bytes_read);
+            file_pos += try print_output(stdout, print_params, &file_contents_buffer, file_pos);
         }
         try stdout.print("\n", .{});
     }
